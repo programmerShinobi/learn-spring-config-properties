bd9e59b59587ae38bdc2ab52b3c8382ac14c8077
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-05 09:10:17 +0700

    first-commit

---
+++ src/main/java/com/programmershinobi/spring/config/BelajarSpringConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.programmershinobi.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringConfigPropertiesApplication.class, args);
+	}
+
+}
