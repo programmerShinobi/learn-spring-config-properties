fa0d1f76afc50c75219764d8816ba793bc00351c
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-09 11:31:17 +0700

    [add] learn : configuration properties

---
+++ src/main/java/com/programmershinobi/spring/config/properties/ApplicationProperties.java
@@ -0,0 +1,18 @@
+package com.programmershinobi.spring.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("application")
+public class ApplicationProperties {
+
+    private String name;
+
+    private Integer version;
+
+    private boolean productionMode;
+
+}
---
+++ src/test/java/com/programmershinobi/spring/config/configurationproperties/ConfigurationPropertiesTest.java
@@ -0,0 +1,32 @@
+package com.programmershinobi.spring.config.configurationproperties;
+
+import com.programmershinobi.spring.config.properties.ApplicationProperties;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ConfigurationPropertiesTest.TestApplicatiom.class)
+public class ConfigurationPropertiesTest {
+
+    @Autowired
+    private ApplicationProperties properties;
+
+    @Test
+    void testConfigurationProperties() {
+        Assertions.assertEquals("Belajar Spring Boot", properties.getName());
+        Assertions.assertEquals(1, properties.getVersion());
+        Assertions.assertEquals(false, properties.isProductionMode());
+    }
+
+    @SpringBootApplication
+    @EnableConfigurationProperties({
+            ApplicationProperties.class
+    })
+    public static class TestApplicatiom {
+
+    }
+
+}
